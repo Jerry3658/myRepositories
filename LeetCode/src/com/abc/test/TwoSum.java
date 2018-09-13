db965fdfdba196b301c1ad2b8752d55c2e77cd53
Jerry3658 <hujie3658@163.com>
2018-09-13 11:11:07 +0800

    commit

--- LeetCode/src/com/abc/test/TwoSum.java
+++ LeetCode/src/com/abc/test/TwoSum.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * @author Jerry
  *
  */
-public class Solution {
+public class TwoSum {
 	public static int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length - 1; i++){
             for(int j = i + 1; j < nums.length; j++){
