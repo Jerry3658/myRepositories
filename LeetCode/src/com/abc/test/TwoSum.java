b7acabad8aebb73b3c92d2e5d984f802f90bcef0
Jerry3658 <hujie3658@163.com>
2018-09-13 11:12:51 +0800

    commit

--- LeetCode/src/com/abc/test/TwoSum.java
+++ LeetCode/src/com/abc/test/TwoSum.java
@@ -10,18 +10,18 @@ import java.util.Arrays;
  */
 public class TwoSum {
 	public static int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length - 1; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                	return new int[] {i,j};
-                }
-            }
-        }
-        throw new IllegalArgumentException("No two sum solution");
-    }
+		for (int i = 0; i < nums.length - 1; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		throw new IllegalArgumentException("No two sum solution");
+	}
 	public static void main(String[] args) {
-		int[] arr = {2,7,11,15};
-		int[] twoSum = twoSum(arr,9);
+		int[] arr = { 2, 7, 11, 15 };
+		int[] twoSum = twoSum(arr, 9);
 		/*打印数组的另一种方式
 		System.out.print("[");
 		for(int i = 0; i < twoSum.length; i++) {
