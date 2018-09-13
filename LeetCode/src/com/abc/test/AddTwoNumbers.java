979aeb199f414298823f29cf8221ec96518db826
Jerry3658 <hujie3658@163.com>
2018-09-13 13:31:03 +0800

    commit

--- LeetCode/src/com/abc/test/AddTwoNumbers.java
+++ LeetCode/src/com/abc/test/AddTwoNumbers.java
@@ -9,7 +9,7 @@ package com.abc.test;
  *
  */
 public class AddTwoNumbers {
-	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 	    ListNode dummyHead = new ListNode(0);
 	    ListNode p = l1, q = l2, curr = dummyHead;
 	    int carry = 0;
